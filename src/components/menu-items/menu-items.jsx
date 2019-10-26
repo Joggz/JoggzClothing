ddae427ea920031f8f4c953b0d76c5cb956ccc57
Idris <idrisaboladeo@gmail.com>
2019-10-26 15:30:54 +0100

    creates project with create-reacte-app: - structured components - adds files for the homepage

---
+++ src/components/menu-items/menu-items.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './menu-items.styles.scss';
+const menuItem = ({title, imageUrl, size}) => (
+    <div style={ {
+        backgroundImage: `URL(${imageUrl})`,
+    } } className={`${size} menu-item`}>
+          <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+);
+
+export default menuItem;
---
+++ src/container/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Directory from '../components/directory/directory.component';
+class App extends Component {
+
+  render(){
+    return (
+      <div >
+        <Directory />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
