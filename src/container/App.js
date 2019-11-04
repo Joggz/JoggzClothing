3519cec6045623a407067d735484ce7f42fa9862
Idris <idrisaboladeo@gmail.com>
2019-11-04 13:42:36 +0100

    adds header component and links to home, shoppage and contact

--- src/container/App.js
+++ src/container/App.js
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
 
+import './App.css';
 import HomePage from '../pages/homepage/homepage.component';
-import ShopPage from '../pages/ShopPage/ShopPage';
-
 import { Switch, Route} from 'react-router-dom';
+import Header from '../components/Header/header.component';
+// import Directory from '../components/directory/directory.component';
 
-import '../container/App.css';
 
-
-const Hatspage = ( props) => {
-  console.log(props)
-  return(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  )
-  };
-
-const Sneakers = ( ) => (
-  <div>
-    <h1>Sneakers PAGE</h1>
-  </div>
-);
 function App() {
+
+  const Hatspage = () => (
+    <div>
+      HATPAGE
+    </div>
+  )
+
+  const Sneakers = () => (
+    <div>
+      Sneakers
+    </div>
+  )
 
     return (
       <div >
+        <Header />
         {/* USED CDN ON HERE AND IT WORKED, CHECK TOP CORNER OF BROWSER */}
         <Switch>
           <Route exact path='/' component={ HomePage } />
-          <Route exact path='/shop' component={ ShopPage } />
-          
+          <Route exact path='/hats' component={ Hatspage } />
+          <Route exact path='/Sneakers' component={ Sneakers } />
           
         </Switch>
       </div>
